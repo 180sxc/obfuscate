a4779c1a9a35171e7847e4c94934cb9195ca8601
180sxc <xcsplgk@gmail.com>
2024-09-10 11:21:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+function init () {
+  var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+    lineNumbers: true,
+    mode: "javascript",
+    indentUnit: 4,
+    indentWithTabs: true,
+    tabSize: 4,
+    autoCloseBrackets: true,
+    matchBrackets: true,
+    styleActiveLine: true,
+    theme: "dracula"
+  });
+}
 function getScript () {
   let input = document.getElementById("mainInput")
 }
