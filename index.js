d56e3a7c400b3b7f3ba69ce9600f9b718e3e2803
180sxc <xcsplgk@gmail.com>
2024-09-10 11:46:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,5 +15,24 @@ init();
 function getScript () {
   let input = document.getElementById("mainInput")
 }
-function obfuscate_V1 (code) {
+
+class V1 {
+  constructor (code){
+    this.code = code
+    this.variables = {};
+  }
+  getVariables (code) {
+   const regex = /\b(var|let|const)\s+([\w$]+)(\s*=\s*(.*))?;/g;
+   const variables = {};
+
+   let match;
+   while ((match = regex.exec(code)) !== null) {
+     const variableName = match[2];
+     const initialValue = match[4];
+     variables[variableName] = initialValue;
+   }
+   return variables;
+  }
+}
+function obfuscate (code) {
 }
