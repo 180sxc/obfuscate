cbe195d9f27c82766dc6e82259d37875e4d1efbf
180sxc <xcsplgk@gmail.com>
2024-09-10 12:54:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-function init () {
-  var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-    lineNumbers: true,
-    mode: "javascript",
-    indentUnit: 4,
-    indentWithTabs: true,
-    tabSize: 4,
-    autoCloseBrackets: true,
-    matchBrackets: true,
-    styleActiveLine: true,
-    theme: "nord"
-  });
-}
-init();
 function getScript () {
   let input = document.getElementById("mainInput")
 }
@@ -22,16 +8,12 @@ class V1 {
     this.variables = {};
   }
   getVariables (code) {
-   const regex = /\b(var|let|const)\s+([\w$]+)(\s*=\s*(.*))?;/g;
-   const variables = {};
-
-   let match;
-   while ((match = regex.exec(code)) !== null) {
-     const variableName = match[2];
-     const initialValue = match[4];
-     variables[variableName] = initialValue;
-   }
-   return variables;
+    const regex = /\b(var|let|const)\s+([\w$]+)(\s*=\s*(.*))?;/g;
+    let tmpVar = code.match(regex);
+    tmpVar.forEach(e => {
+      this.variables[e] = 1
+    })
+   return this.variables;
   }
 }
 function obfuscate (code) {
