fde8eaa5a38301fbf762c4da480a6401e1f916dd
180sxc <xcsplgk@gmail.com>
2024-09-12 15:29:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-function getScript () {
-  let input = document.getElementById("mainInput");
-  let code = input?.value;
-  return code;
-}
+addEventListener("DOMContentLoaded", (event) => {
+  function getScript() {
+    let input = document.getElementById("mainInput");
+    let code = input?.value;
+    return code;
+  }
 
-class V1 {
-  constructor (code){
-    this.code = code
-    this.variables = {};
+  class V1 {
+    constructor(code) {
+      this.code = code
+      this.variables = {};
+    }
+    getVariables(code) {
+      const regex = /\b(var|let|const)\s+([\w$]+)(\s*=\s*(.*))?;/g;
+      let tmpVar = code.match(regex);
+      tmpVar.forEach(e => {
+        this.variables[e] = 1
+      })
+      return this.variables;
+    }
+    skimmed(code) {
+      return code.replaceAll("\n", ";")
+    }
+    output() {
+      let tmp = this.code;
+      let skim = this.skimmed(tmp);
+      return skim;
+    }
   }
-  getVariables (code) {
-    const regex = /\b(var|let|const)\s+([\w$]+)(\s*=\s*(.*))?;/g;
-    let tmpVar = code.match(regex);
-    tmpVar.forEach(e => {
-      this.variables[e] = 1
-    })
-   return this.variables;
+
+  function obfuscate(code) {
+    let newCode = new V1(code);
+    return newCode.output();
   }
-  skimmed (code) {
-    return code.replaceAll("\n", ";")
+  document.getElementById("start").onclick = function() {
+    let code = getScript();
+    let obfuscated = obfuscate(code);
+    document.getElementById("mainInput").value = obfuscated;
   }
-  output () {
-    let tmp = this.code;
-    let skim = this.skimmed(tmp);
-    return skim;
-  }
-}
-function obfuscate (code) {
-  let newCode = new V1(code);
-  return newCode.output();
-}
-document.getElementById("start").onclick = function(){
-  let code = getScript();
-  let obfuscated = obfuscate(code);
-  document.getElementById("mainInput").value = obfuscated;
-}
+});
